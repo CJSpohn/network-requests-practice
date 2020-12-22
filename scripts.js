2fb67201e68b1717e0f04034e2d19a959ad0390a
Chris Spohn <chris.j.spohn@gmail.com>
2020-12-22 14:37:28 -0700

    Create fetch structure

--- scripts.js
+++ scripts.js
@@ -6,18 +6,40 @@ const usersBtn = document.querySelector('.js-users-btn');
 const teamsBtn = document.querySelector('.js-teams-btn');
 const animalsBtn = document.querySelector('.js-animals-btn');
 
+const addDataBtn = document.querySelector('.js-add-data-btn')
+
 const usersClearBtn = document.querySelector('.js-users-clear');
 const teamsClearBtn = document.querySelector('.js-teams-clear');
 const animalsClearBtn = document.querySelector('.js-animals-clear');
 
-console.log('test')
+const userId = document.querySelector('#id');
+const userName = document.querySelector('#name');
+const userStatus = document.querySelector('#status');
+const userInterests = document.querySelector('#interests');
 
-function getData(api) {
-  const promise = fetch(api)
+
+function getData(url) {
+  const promise = fetch(url)
     .then(response => response.json())
     .then(data => data)
 
   return promise
+}
+
+function postData(url, data) {
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      id: data.id,
+      name: data.name,
+      status: data.status,
+      interest: data.intersts
+    })
+  })
+    .then(response => response.json())
 }
 
 const getUsersData = async () => {
@@ -60,6 +82,17 @@ function clearData()  {
   }
 }
 
+function makeData() {
+  const item = {
+    id: userId.value,
+    name: userName.value,
+    status: userStatus.value,
+    interests: userInterests.value
+  }
+  postData('http://localhost:3001/api/v1/users', item)
+}
+
+
 
 usersBtn.addEventListener('click', getUsersData)
 teamsBtn.addEventListener('click', getTeamsData)
@@ -68,3 +101,5 @@ animalsBtn.addEventListener('click', getAnimalsData)
 usersClearBtn.addEventListener('click', clearData)
 teamsClearBtn.addEventListener('click', clearData)
 animalsClearBtn.addEventListener('click', clearData)
+
+addDataBtn.addEventListener('click', makeData)
